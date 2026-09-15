691ca367c13fa23e2a2c2d0c5773ac51ed20250d
PremKR2505 <pklegendary25@gmail.com>
2026-09-16 00:23:41 +0530

    started eventloggercli class

--- Eventloggercli.java
+++ Eventloggercli.java
@@ -8,3 +8,13 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+public class Eventloggercli {
+    private static final String LOG_FILE = "event_log.txt";
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    System.out.println("Welcome to the Event Logger it runs quite simply in the command line!");
+    
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
